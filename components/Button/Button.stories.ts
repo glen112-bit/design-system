dec1829a89625c4abd8a88dc90172ed84d6be226
Glen112 <escobar.glen@gmail.com>
2024-04-23 18:38:53 -0300

    variants

--- components/Button/Button.stories.ts
+++ components/Button/Button.stories.ts
@@ -25,6 +25,48 @@ export const Primary: StoryObj<ButtonProps> = {
     children: 'Botão'
   }
 }
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Botão',
+    variant: 'tertiary'
+  }
+}
+export const TertiaryDisabled: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Botão',
+    variant: 'tertiary',
+    disabled: true
+  }
+}
+export const VioletTertiary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Botão',
+    className: 'theme-violet',
+    variant: 'tertiary'
+  }
+}
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Botão',
+    disabled: true
+  }
+}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Botão',
+    variant: 'secondary'
+  }
+}
+
+export const SecondaryDisabled: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Botão',
+    variant: 'secondary',
+    disabled: true
+  }
+}
 
 export const Violet: StoryObj<ButtonProps> = {
   args: {
@@ -32,3 +74,12 @@ export const Violet: StoryObj<ButtonProps> = {
     className: 'theme-violet'
   }
 }
+
+export const VioletSecondary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Botão',
+    className: 'theme-violet',
+    variant: 'secondary'
+  }
+}
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import Button from "@/components/Button/Button"
 
 const App = () => {
-  return <Button>Botão</Button>
+  return <>
+      <Button>Botão</Button>
+      <Button variant="secondary">
+        Botão
+      </Button>
+    </>
 }
 
 export default App
